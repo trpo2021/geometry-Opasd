4967631c96f6baded2572e6bc56c64d7869c7b5c
Клепов Степан Дмитриевич <ip014s14@bwp15.csc.local>
2021-02-17 18:56:27 +0700

    geometry_begin

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-  printf("Hello, World");
+  double x1,y1,x2,y2,perimetr,dlina;
+  scanf ("%lf ,%lf ,%lf ,%lf",&x1 , &y1 , &x2 , &y2);
+  dlina = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+  perimetr = 2*3.14*dlina;
+  printf ("%lf",perimetr);
   return 0;
 }
