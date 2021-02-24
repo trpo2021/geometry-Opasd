4936bce14dd633f72bcd69b4600ee1d641d241c4
Клепов Степан Дмитриевич <ip014s14@bwp11.csc.local>
2021-02-24 18:26:22 +0700

    area and perimeter

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+float const Pi=3.1415
 int main()
 {
-  double x1,y1,x2,y2,perimetr,dlina;
-  scanf ("%lf ,%lf ,%lf ,%lf",&x1 , &y1 , &x2 , &y2);
-  dlina = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-  perimetr = 2*3.14*dlina;
-  printf ("%lf",perimetr);
+  float x,y,r,S,P;
+  scanf("%f",&x);
+  scanf("%f",&y);
+  scanf("%f",&r);
+  P=2*Pi*r;
+  S=Pi*2*2;
+  printf ("perimeter=%f\n",P);
+  printf ("area=%f\n",S);
   return 0;
 }
