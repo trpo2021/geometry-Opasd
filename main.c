a68ba5053f3000bdd5e1c495cfdd3c2dbe92be0f
Клепов Степан Дмитриевич <ip014s14@bwp11.csc.local>
2021-03-31 17:44:51 +0700

    fix_n<0

--- main.c
+++ main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
 float const Pi=3.1415;
 int main()
 {
-  
   float x1,y1,x2,y2,x3,y3,r,S,P,Pir,Area,a,b,c,n,cen1,cen2,colvo1,colvo2;
+  do
+    {
+      printf ("colvo_circle=");
+      scanf("%f",&colvo1);
+  if (colvo1<=0)
+    printf ("err_colvo<=0\n");
+    }
+  while (colvo1<=0);
 
-  printf ("colvo_circle=");
-  scanf("%f",&colvo1);
+  do
+    {
   printf ("colvo_triangle=");
   scanf("%f",&colvo2);
+  if (colvo2<=0)
+    printf ("err_colvo<=0\n");
+      }
+  while (colvo2<=0);
+  
 
   do
     {
