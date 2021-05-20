a69871ccb651001910f486b1b2e340f146d22509
Opasd <78478987+Opasd@users.noreply.github.com>
2021-05-20 16:04:50 +0700

    src

--- src/lib/figure.h
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-
-float per_cir (float r);
-float area_cir (float r);
-float per_tri (float x1,float y1,float x2 ,float y2,float x3,float y3);
-float area_tri (float x1,float y1,float x2 ,float y2,float x3,float y3);
-
